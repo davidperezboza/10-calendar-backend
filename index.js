e09f08bd344eebc81a639d0751e3e351e9c510f3
davidperezboza <david.perez@babelgroup.com>
2025-02-12 14:18:48 +0100

    Video 376

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 
-//Crear el servidor de express
+// Crear el servidor de express
 const app = express();
 
-//Directorio público
+// Servir directorio público
 app.use(express.static('public'));
 
-//Escuchar peticiones
-app.listen(process.env.PORT, () =>{
+// Middleware de rutas
+app.use('/api/auth',  require("./routes/auth"));
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
